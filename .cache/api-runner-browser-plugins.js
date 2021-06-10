26506d7359d0e2ae12f5c844c5090b01c14a7f23
Kasper Fabricius Kristensen <kafk@itu.dk>
2021-06-10 14:29:12 +0200

    removing folders

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-image/gatsby-browser.js'),
-      options: {"plugins":[]},
-    },{
-      plugin: require('../node_modules/gatsby-plugin-react-redux/gatsby-browser.js'),
-      options: {"plugins":[],"pathToCreateStoreModule":"./src/state/createStore","serialize":{"space":0,"isJSON":true,"unsafe":false,"ignoreFunction":true},"cleanupOnClient":true,"windowKey":"__PRELOADED_STATE__"},
-    }]
